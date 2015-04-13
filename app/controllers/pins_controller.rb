de889e3d9da0f12093b6a99acdc21e7b351e151e
Ant <alonezuko@mail.ru>
2015-04-13 22:23:46 +0600

    add page for view pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,13 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
+  def show
+  end
+
+  end
   def new
     @pin = Pin.new
   end
@@ -19,6 +25,10 @@ class PinsController < ApplicationController
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
 
   end
 end
